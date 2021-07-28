66bad5d19beb479a6ec7a1df4bad62eab009205f
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 12:56:36 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+
+ public static void main (String [] args){
+
+
+     String day = "pizza";
+
+     switch (day){
+         case "sunday": System.out.println("Its Sunday");
+         break;
+
+         case "monday": System.out.println("IT is Monday");
+         break;
+         case "saturday": System.out.println("Its saturday");
+             break;
+
+         case "tuesday": System.out.println("IT is tuesday");
+             break;
+         case "thursday": System.out.println("Its thursday");
+             break;
+
+         case "friday": System.out.println("It is friday");
+             break;
+
+         case "wednesday": System.out.println("It is wednesday");
+             break;
+
+         default : System.out.println("its not a day");
+
+
+
+     }
+
+
+
+
+
+
+ }
+
+
+}
